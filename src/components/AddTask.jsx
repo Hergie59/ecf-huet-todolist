030f78e30ec499d30f72c8c2b290f8e857c7c788
Eloise Huet (alias: Hergie) <Hergie59410@gmail.com>
2022-07-04 16:35:35 +0200

    décommentage du test + modification du test

--- src/components/AddTask.jsx
+++ src/components/AddTask.jsx
@@ -1,27 +1,34 @@
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import { FaPlusSquare } from 'react-icons/fa';
 
 
 function AddTask() {
-    // const [tasks, setTask ] = useState([]);
-    // const [valeurText, setValeurText] = useState("");
-    // const task = { id: Math.floor(Math.random() * 1000), name: valeurText };
+    const [tasks, setTask ] = useState([]);
+    const [valeurText, setValeurText] = useState("");
+    const task = { id: Math.floor(Math.random() * 1000), name: valeurText };
+    
+    function handleChange(e){
+        setValeurText(e.target.value);
+        e.preventDefault();    
+    }
 
-    // function handleChange(e){
-    //     setValeurText(e.target.value);
-    //     setValeurText((prevState) => [...prevState, task]);    
-    // }
-
-    // function handleClick(){
-    //     setTask((prevState) => [...prevState, task]);
-    // }
+    function handleClick(){
+        setTask((prevState) => [...prevState, task]);
+    }
 
     return(
         <>
         <h3>Nouvelle tâche</h3>
         <div>
-            <input type="text" placeholder="Add newTask" /*value={valeurText} onChange={handleChange}*//>
-            <button /*onClick={handleClick}*/><FaPlusSquare /></button>
+            <ul>
+            {tasks.map((task) => (
+                <li>{task}</li>
+            ))}
+            </ul>
+        </div>
+        <div>
+            <input type="text" placeholder="Add newTask" value={valeurText} onChange={handleChange}/>
+            <button onClick={handleClick}><FaPlusSquare /></button>
         </div>
         </>
     );  
